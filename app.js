cde7519ceeba3d371119ef03cb0b6b7ca6b06c6c
David Leda <davmail99@yahoo.com>
2023-05-22 15:44:07 -0300

    1st

---
+++ app.js
@@ -0,0 +1,15 @@
+
+import ComponenteUno from './components/componenteUno.js'
+
+const { createApp } = Vue
+
+createApp({
+
+  components: {
+    ComponenteUno
+  }
+
+}).mount('#app')
+
+
+
---
+++ components/componenteUno.js
@@ -0,0 +1,20 @@
+
+
+export default {
+
+  template: `
+
+  <nav>
+    <div>
+      <p>Item 1</p>
+    </div>
+    <div>
+      <p>Item 2</p>
+    </div>
+    <div>
+      <p>Item 3</p>
+    </div>
+  </nav>
+  
+  `
+}
